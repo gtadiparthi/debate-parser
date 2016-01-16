78fd98cf79d08db16b4c2afaa4539f72aac34a87
Gopalakrishna Reddy Tadiparthi <tadiparthi@gmail.com>
2016-01-16 06:33:43 -0800

    Updated with transcript from the 6th debate

--- Merge_sentiments.py
+++ Merge_sentiments.py
@@ -6,20 +6,29 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.style.use('ggplot')
 
-debate = pd.read_csv("rep_debate3_output.csv")
-sentiment = pd.read_csv("rep_debate_sentiment.csv")
-data = pd.merge(debate,sentiment,on='SentenceNo')
+sentiment1 = pd.read_csv("all_debates_PAsent.csv")
+sentiment2 = pd.read_csv("all_debates_NBsent.csv")
+#Deleting (dropping) certain columns from the dataframe
+del sentiment2['Text']
+del sentiment2['Speaker']
+del sentiment2['SequenceNo']
+
+sentiment3 = pd.read_csv("all_debates_snlpsent.csv")
+
+data = pd.merge(sentiment1,sentiment2,on=['Party', 'DebateNo', 'SentenceNo'])
+data = pd.merge(data,sentiment3, on = ['Party', 'DebateNo', 'SentenceNo'])
+
 data['wc'] = 0
 # If a sentence has less than 3 words, force it to be neutral
-for index, row in data.iterrows():
-    score1 = row['Score']
-    text = str(row['Text'])
-    wordcount = len(text.split(None)) 
-    #print index, " ", text, " ",wordcount
-#    print data.ix[index,'wc']
-    data.ix[index,'wc']= wordcount
-    if wordcount < 4 and score1 <2:
-    	data.ix[index,'Score']= 2
+# for index, row in data.iterrows():
+#     score1 = row['Score']
+#     text = str(row['Text'])
+#     wordcount = len(text.split(None)) 
+#     #print index, " ", text, " ",wordcount
+# #    print data.ix[index,'wc']
+#     data.ix[index,'wc']= wordcount
+#     if wordcount < 4 and score1 <2:
+#     	data.ix[index,'Score']= 2
 
 #Combining the raw scores for each sentence and aggregate into a paragraph
 # data1=data.groupby(['SequenceNo'])['Score'].mean().reset_index()
@@ -30,10 +39,10 @@ for index, row in data.iterrows():
 # print(data2.head(5))
 # data12 = pd.merge(data1,data2,on='SequenceNo')
 # data = pd.merge(data3, data12, on ='SequenceNo')
-data = data [~data.Speaker.isin(['MALE','SANTELLI','(UNKNOWN)','UNIDENTIFIED MALE','HARMAN', 'HARWOOD','CRAMER','EPPERSON','QUICK','QUINTANILLA'])]
-print(data)
-ax = data.boxplot(column='Score', by = 'Speaker')
-fig = ax.get_figure()
-fig.savefig('figure.png')
-data.to_csv("rep_debate3_final.csv", index_col = False)
+#data = data [~data.Speaker.isin(['MALE','SANTELLI','(UNKNOWN)','UNIDENTIFIED MALE','HARMAN', 'HARWOOD','CRAMER','EPPERSON','QUICK','QUINTANILLA'])]
+#print(data)
+#ax = data.boxplot(column='Score', by = 'Speaker')
+#fig = ax.get_figure()
+#fig.savefig('figure.png')
+data.to_csv("all_debates_all_sents.csv", index = False)
 
